17bf4e62990862917f357ed86eea9da12755c523
Ritesh <riteshtiwari3112003@gmail.com>
2024-12-27 19:49:09 +0530

    this is my dashboard 56 commit

--- src/index.js
+++ src/index.js
@@ -8,9 +8,10 @@ const { Pool } = pkg;
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+console.log(process.env.DATABASE_URL);
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  
   ssl: {
     rejectUnauthorized: false
   }
